7f72ce554e8c0e2ebf0585135ce60fb31fdaf359
spandrel64 <spandrel64@gmail.com>
2016-06-04 15:42:04 -0400

    finished project

--- js/app.js
+++ js/app.js
@@ -2,12 +2,20 @@ $(document).ready(function() {
 $('ul').on('click', '.fa-close', function(event){
 	$(this).parent().remove();
 });
-$('ul').on('click', '.fa-arrow-right', function(event){
-	$(this).parent().appendTo('ul');
-	//$(this).parent().remove();
-});
-$('.add').on('click','.add', function(event){
-	$(this).alertBox();
+$('.main-items ul').on('click', '.fa-arrow-right', function(event){
+	$('.on-boat ul').append($(this).parent());
+
+}); 
+$('.on-boat ul').on('click', '.fa-arrow-right', function(event){
+	$('.main-items ul').append($(this).parent());
+
+}); 
+
+//add function to move from .on-boat to .main-items
+
+$('.add').on('click', function(event){
+	var item= prompt('Please add item');
+	$('.main-items ul').append('<li><span>'+item+'</span><i class="fa fa-close"></i><i class="fa fa-arrow-right"></i></li>');
 });
 
 });
